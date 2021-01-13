322c984a4f94353230980632dcc50d7b627783dc
BobbySinclusto <allencbenjamin@gmail.com>
2021-01-12 17:59:32 -0800

    added a ton of stuff

--- bot.py
+++ bot.py
@@ -11,24 +11,25 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import wolframalpha
 
+import io
+import aiohttp
+
+from PIL import Image
+from googleapiclient.discovery import build
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
-SCRIPTS_PATH = os.getenv('SCRIPTS_PATH')
-SERVER_ADDRESS = os.getenv('SERVER_ADDRESS')
-SERVER_PORT = os.getenv('SERVER_PORT')
 WOLFRAM_API_KEY = os.getenv('WOLFRAM_API_KEY')
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+GOOGLE_SE_ID = os.getenv('GOOGLE_SE_ID')
 
 try:
     wolfram_client = wolframalpha.Client(WOLFRAM_API_KEY)
 except:
     pass
 
-current_world = open(SCRIPTS_PATH + 'currentworld.txt', 'r').read().strip()
 funfacts = open('dyk.txt', 'r').readlines()
-server_process = 0
-ngrok_process = 0
-addr = ''
 
 bot = commands.Bot(command_prefix='!')
 @bot.event
@@ -40,13 +41,18 @@ async def on_ready():
 
     members = '\n - '.join([str(member.id) + ' ' + member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
-    game = discord.Game('ngrok')
+    game = discord.Game('Wolfram|Alpha')
     await bot.change_presence(status=discord.Status.online, activity=game)
 
 @bot.event
 async def on_member_join(member):
     await member.create_dm()
-    await member.dm_channel.send(f'Hi {member.name}, welcome to the Nerd Herd!')
+    await member.dm_channel.send(f'Hi {member.name}, welcome to Hacky Stack!')
+
+def google_search(search_term, api_key, cse_id, **kwargs):
+    service = build("customsearch", "v1", developerKey=api_key)
+    res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
+    return res['items']
 
 @bot.event
 async def on_message(message):
@@ -59,10 +65,57 @@ async def on_message(message):
         questions = ['hey bob', '?', 'who', 'what', 'when', 'where', 'why', 'how', 'which']
         if any(q in msg for q in questions):
             try:
+                if 'image' in msg:
+                    raise Exception('use google')
                 msg = msg.replace('hey bob, ', '').replace('hey bob', '').replace(', bob', '').replace('bob', '').replace('?', '') 
                 res = wolfram_client.query(msg)
-                await message.channel.send(next(res.results).text.replace('Wolfram|Alpha', 'Bob McFoggarty'))
+                try:
+                    await message.channel.send(next(res.results).text.replace('Wolfram|Alpha', 'Bob McFoggarty'))
+                except:
+                    pass
+                
+                async with aiohttp.ClientSession() as session:
+                    for p in res.pods:
+                        for s in p.subpods:
+                            try:
+                                async with session.get(s['img']['@src']) as resp:
+                                    if resp.status == 200:
+                                        data = io.BytesIO(await resp.read())
+                                        await message.channel.send(file=discord.File(data, 'result.png'))
+                            except:
+                                pass
+                        
             except:
+                try:
+                    msg = msg.replace('hey bob, ', '').replace('hey bob', '').replace(', bob', '').replace('bob', '').replace('?', '') 
+                    if 'image' in msg or 'animated gif' in msg:
+                        results = google_search(msg + ' filetype:png OR filetype:gif OR filetype:jpg OR filetype:jpeg', GOOGLE_API_KEY, GOOGLE_SE_ID, searchType='image', num=10)
+                        async with aiohttp.ClientSession() as session:
+                            random.shuffle(results)
+                            for result in results:
+                                print('started trying')
+                                try:
+                                    async with session.get(result['link']) as resp:
+                                        if resp.status == 200:
+                                            data = io.BytesIO(await resp.read())
+                                            print('started sending ' + result['fileFormat'])
+
+                                            await message.channel.send(file=discord.File(data, 'result.' + re.search(r'image/(.+$)', result['fileFormat']).groups()[0]))
+                                        else:
+                                            raise Exception('unable to get image')
+                                        return
+                                except Exception as e:
+                                    print(e)
+                    else:
+                        results = google_search(msg, GOOGLE_API_KEY, GOOGLE_SE_ID, num=1)
+                        for result in results:
+                            print(result)
+                            await message.channel.send(result['snippet'])
+                    return
+
+                except Exception as e:
+                    print(e)
+
                 members = [133636765649993729, 236675869643505665, 346401802910040076, 348596653735018496, 472165066670735363, 487262355546177536, 604427812895588363]
                 opt = random.randrange(3)
                 if opt == 0:
@@ -86,145 +139,78 @@ async def on_command_error(ctx, error):
         await ctx.send('Error processing command. See !help for command usage instructions.')
     await ctx.send('Did you know ' + random.choice(funfacts))
 
+async def make_ban_img(user_img):
+    ban_imgs = [Image.open('assets/ban%d.png' % (i+1)).convert('RGBA') for i in range(5)]
+
+    # new size is 60x60, starts at 150, 159
+
+
+    avatar_img = Image.open(user_img)
+    avatar_img = avatar_img.resize((60, 60))
+
+    avatar_img_r = Image.new(avatar_img.mode, (ban_imgs[0].width, ban_imgs[1].height))
+    avatar_img_r.paste(avatar_img, (150, 159))# 210, 219))
+    avatar_img_r.putpalette(avatar_img.palette.getdata()[1])
+
+    frames = []
+
+    for frame in ban_imgs:
+        to_cover = avatar_img_r.convert('RGBA')
+        to_cover.paste(frame, mask=frame)
+        frames.append(to_cover)
+
+    out = io.BytesIO()
+
+    frames[0].save(out, 'gif', save_all=True, append_images=frames[1:], loop=0, duration=90)
+    print('opened images')
+    out.seek(0)
+    return out
+
+
+@bot.command(name='ban', help='bans a member from the server')
+async def ban(ctx, *args):
+    try:
+        userids = [int(r) for r in re.findall(r'<@!{,1}([0-9]*)>', ' '.join(args))]
+        for userid in userids:
+            user = None
+            async for member in ctx.guild.fetch_members(limit=None):
+                if member.id == userid:
+                    user = member
+                    break
+
+            pfp = str(user.avatar_url).split('?size=')[0]
+            async with aiohttp.ClientSession() as session:
+                try:
+                    async with session.get(pfp) as resp:
+                        print(resp)
+                        if resp.status == 200:
+                            original_img = io.BytesIO(await resp.read())
+                            pil_img = Image.open(original_img)
+                            pil_img.info.pop('background', None)
+                            data = io.BytesIO()
+                            pil_img.save(data, 'gif')
+                            data.seek(0)
+
+                            res = await make_ban_img(data)
+
+                            print('got image')
+                            await ctx.channel.send(file=discord.File(res, 'result.gif'))
+                            print('sent')
+                        else:
+                            raise Exception('download failed')
+                except:
+                    pass
+        
+    except Exception as e:
+        await ctx.send('Banned.')
+        print(e)
+
 @bot.command(name='ping', help='pong!')
 async def ping(ctx):
     await ctx.send('pong!')
 
-@bot.command(name='startserver', help='Starts the server')
-async def startserver(ctx):
-    global server_process
-    global ngrok_process
-    global addr
-    if addr != '':
-        await ctx.send('The server is running already! Server address: ' + addr)
-        return
-    await ctx.send('Starting server...')
-        
-    server_process = subprocess.Popen(['java', '-Xmx1024M', '-Xms1024M', '-jar', 'server.jar', 'nogui'], cwd=SCRIPTS_PATH, stdin=subprocess.PIPE) 
-    if SERVER_ADDRESS == None:
-        ngrok_process = subprocess.Popen([SCRIPTS_PATH + 'ngrok', 'tcp', str(SERVER_PORT)], cwd=SCRIPTS_PATH, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        while True:
-            try:
-                time.sleep(2)
-                j = requests.get('http://127.0.0.1:4040/api/tunnels').json()
-                addr = j['tunnels'][0]['public_url'][6:]
-                break
-            except:
-                pass
-    else:
-        addr = SERVER_ADDRESS
-    await ctx.send('Success! Server address: ' + addr)
-
-@bot.command(name='currentworld', help='Displays the currently selected world')
-async def currentworld(ctx):
-    await ctx.send('Current world: ' + current_world)
-
-@bot.command(name='serveraddress', help='Displays the server address')
-async def serveraddress(ctx):
-    if addr == '':
-        await ctx.send('The server is not running yet. Use !startserver to start it!')
-    else:
-        await ctx.send(addr)
-
-@bot.command(name='stopserver', help='Stops the server')
-async def stopserver(ctx):
-    global addr
-    try:
-        server_process.communicate(input=b'/stop\n')
-        if SERVER_ADDRESS == None:
-            ngrok_process.terminate()
-        addr = ''
-        await ctx.send('Server stopped.')
-    except:
-        await ctx.send('You can\'t stop a server that hasn\'t been started. Use !startserver to start the server!')
-
-@bot.command(name='resetworld', help='Resets world to its original state')
-async def resetworld(ctx):
-    global server_process
-    await ctx.send('Resetting the world...')
-    try:
-        server_process.communicate(input=b'/stop\n')
-    except:
-        print(sys.exc_info()[0])
-        pass
-    subprocess.call('rm -rf ' + SCRIPTS_PATH + 'world/*', shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
-    subprocess.call(['ls', SCRIPTS_PATH + 'world'])
-    subprocess.call('cp -r ' + SCRIPTS_PATH + '../Worlds/' + current_world.replace(' ', '\\ ').replace('\'', '\\\'') + '/* ' + SCRIPTS_PATH + 'world/', shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True) 
-    if addr == '':
-        await ctx.send('The server isn\'t started! Use !startserver to start the server.')
-        return
-    server_process = subprocess.Popen(['java', '-Xmx1024M', '-Xms1024M', '-jar', 'server.jar', 'nogui'], cwd=SCRIPTS_PATH, stdin=subprocess.PIPE)
-    await ctx.send('The server is restarting...')
-    time.sleep(5)
-    await ctx.send('Success! Server address: ' + addr)
-
-@bot.command(name='listworlds', help='Lists the worlds that have been added')
-async def listworlds(ctx):
-    p = subprocess.run(['ls', SCRIPTS_PATH + '../Worlds/'], stdout=subprocess.PIPE)
-    msg = '```'
-    worlds = p.stdout.decode("utf-8").split('\n')
-    for i in range(1, len(worlds)):
-        msg += str(i) + '. ' + (' ' if i < 10 else '') + worlds[i - 1] + '\n'
-    msg += '```'
-    await ctx.send(msg)
-
-@bot.command(name='selectworld', help='Selects a world, by world name or world number. Use double quotes around a world name, or a world number from the list of worlds.')
-async def selectworld(ctx, world):
-    global current_world
-    p = subprocess.run(['ls', SCRIPTS_PATH + '../Worlds/'], stdout=subprocess.PIPE)
-    worlds = p.stdout.decode("utf-8").split('\n')
-
-    idx = -1
-    try:
-        idx = int(world) - 1
-    except:
-        for i in range(len(worlds)-1):
-            if world == worlds[i]:
-                idx = i
-
-    if idx < 0 or idx > len(worlds) - 2:
-        await ctx.send('Invalid input.')
-    else:
-        current_world = worlds[idx]
-        f = open(SCRIPTS_PATH + 'currentworld.txt', 'w')
-        f.write(worlds[idx])
-        await ctx.send('Selected world: ' + worlds[idx])
-        await resetworld(ctx)
-        print('select world')
-
-@bot.command(name='addworld', help='Adds a world from a link to a zip file.')
-async def addworld(ctx, link):
-    await ctx.send('Attempting to download world...')
-    try:
-        subprocess.run(['mkdir', SCRIPTS_PATH + 'blorgle'])
-        r = requests.get(link, allow_redirects=True)
-        open(SCRIPTS_PATH + 'blorgle/download.zip', 'wb').write(r.content)
-        p = subprocess.run(['unzip', SCRIPTS_PATH + 'blorgle/download.zip'], cwd=SCRIPTS_PATH + 'blorgle/', stdout=subprocess.PIPE)
-        if p.returncode == 0:
-            world = ''
-            path = ''
-            output = p.stdout.decode('utf-8').split('\n')
-            for i in range(len(output)):
-                output[i] = output[i].strip()
-                match = re.compile('[/| ][^/]*/region').search(output[i])
-                if match != None:
-                    world = output[i][match.start() + 1 : match.end() - 7]
-                    path = SCRIPTS_PATH + 'blorgle/' + output[i][10:match.end() - 7].replace(' ', '\\ ').replace('\'', '\\\'')
-                    break
-            if world == '':
-                await ctx.send('Not a valid Minecraft world :-(')
-            else:
-                subprocess.run('mv ' + path + ' ' + SCRIPTS_PATH + '../Worlds/', shell=True)
-                await ctx.send(world + '\nImported successfully!')
-                await ctx.send('Use !selectworld \"' + world + '\" to select it.')
-        else:
-            await ctx.send('File was not a zip archive :-(')
-    except:
-        await ctx.send('Unable to download file from url :-(')
-        pass
-    subprocess.run(['rm', '-rf', SCRIPTS_PATH + 'blorgle'])
-
 @bot.command(name='funfact', help='Displays a fun fact.')
 async def funfact(ctx):
     await ctx.send('Did you know ' + random.choice(funfacts))
+
 bot.run(TOKEN)
